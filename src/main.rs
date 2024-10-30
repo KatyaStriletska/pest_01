4e8772b855e17ee6c93020e9bf35639e73818dfc
Striletska Kateryna <striletskay2005@gmail.com>
2024-10-30 12:19:23 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use pest_derive:: Parser;
+use pest:: Parser;
+
+#[derive(Parser)]
+#[grammar = "./grammar.pest"]
+pub struct Grammar;
+
+
+fn main() -> anyhow:: Result <()>{
+
+   let got = Grammar:: parse(Rule::field, "-6.98,-14\n")?;
+   println!("{:?}", got);
+   Ok(())
+}
